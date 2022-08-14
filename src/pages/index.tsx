9826d71c1d21977613a748dac6580a901acb10d1
Markus <z0n@users.noreply.github.com>
2022-08-15 01:33:35 +0200

    fix: moved global styles to new Layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,22 +5,25 @@ import { Skills } from '../components/skills/skills'
 import * as styles from './index.module.css'
 import { ExperienceList } from '../components/experiences/experience-list'
 import { Background } from '../components/background/background'
+import { Layout } from '../components/layout/layout'
 
 const IndexPage = (): ReactElement => {
     const cvName = process.env.GATSBY_CV_NAME as string
 
     return (
-        <main>
-            <title>{cvName} - CV</title>
-            <Background>
-                <HeaderBar />
-                <div className={styles.content}>
-                    <AboutMe />
-                    <ExperienceList />
-                    <Skills />
-                </div>
-            </Background>
-        </main>
+        <Layout>
+            <main>
+                <title>{cvName} - CV</title>
+                <Background>
+                    <HeaderBar />
+                    <div className={styles.content}>
+                        <AboutMe />
+                        <ExperienceList />
+                        <Skills />
+                    </div>
+                </Background>
+            </main>
+        </Layout>
     )
 }
 
